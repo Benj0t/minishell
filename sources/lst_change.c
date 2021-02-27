8fd7534adc51ad281aa09c37f8d1af44278494d2
psemsari <psemsari@student.42.fr>
2021-02-27 12:44:42 +0100

    fix

--- sources/lst_change.c
+++ sources/lst_change.c
@@ -6,29 +6,19 @@
 /*   By: psemsari <psemsari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 10:54:15 by psemsari          #+#    #+#             */
-/*   Updated: 2021/02/27 11:19:46 by psemsari         ###   ########.fr       */
+/*   Updated: 2021/02/27 12:39:22 by psemsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void		egal_replace(char **envp, size_t i, t_list **tmp, t_list **ret)
+t_var_env	*egal_replace(char **envp, size_t i)
 {
 	char		*c;
-	t_var_env	*malloc;
 
 	c = ft_strchr(envp[i], '=');
 	c[0] = '\0';
-	malloc = malloc_varenv(envp[i], &c[1]);
-	if (malloc == NULL)
-		return (NULL);
-	tmp = ft_lstnew(malloc);
-	if (tmp == NULL)
-		return (NULL);
-	if (i == 0)
-		ret = tmp;
-	else
-		ft_lstadd_back(&ret, tmp);
+	return(malloc_varenv(envp[i], &c[1]));
 }
 
 t_list		*envp_to_list(char **envp)
@@ -36,13 +26,23 @@ t_list		*envp_to_list(char **envp)
 	int			i;
 	t_list		*ret;
 	t_list		*tmp;
+	t_var_env	*malloc;
 
 	ret = NULL;
 	tmp = NULL;
 	i = 0;
 	while (envp[i] != NULL)
 	{
-		egal_replace(envp, i, &tmp, &ret);
+		malloc = egal_replace(envp, i);
+		if (malloc == NULL)
+			return (NULL);
+		tmp = ft_lstnew(malloc);
+		if (tmp == NULL)
+			return (NULL);
+		if (i == 0)
+			ret = tmp;
+		else
+			ft_lstadd_back(&ret, tmp);
 		i++;
 	}
 	return (ret);
