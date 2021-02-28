4c2becd1f44375cf492dc471eabd90fb84bed3d4
Benjamin Moreau <bmoreau89000@gmail.com>
2021-02-28 11:18:48 +0100

    pwd fix

--- sources/builtins/pwd.c
+++ sources/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: bemoreau <bemoreau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 20:01:04 by psemsari          #+#    #+#             */
-/*   Updated: 2021/02/23 20:43:50 by bemoreau         ###   ########.fr       */
+/*   Updated: 2021/02/28 11:18:19 by bemoreau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int		ft_pwd(void)
 {
 	char	*pwd;
 
-	pwd = get_env("PWD");
+	pwd = getcwd(NULL, MAXPATHLEN);
 	if (pwd == NULL)
 		return (-1);
 	else
 		ft_putstr_fd(pwd, 1);
 	ft_putchar_fd('\n', 1);
+	free(pwd);
 	return (0);
 }
